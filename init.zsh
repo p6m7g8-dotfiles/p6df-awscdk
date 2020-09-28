3f50217a31b8bbc3fcf2420e9616e8630270a7fc
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-09-28 11:42:28 -0400

    feat(aliases): adds aws-cdk aliases (#1)

--- init.zsh
+++ init.zsh
@@ -43,5 +43,18 @@ p6df::modules::awscdk::home::symlink() { }
 ######################################################################
 p6df::modules::awscdk::init() {
 
-  true
+  p6df::modules::awscdk::aliases
+}
+
+p6df::modules::awscdk::aliases() {
+
+  # runs an npm script via lerna for a the current module
+  alias lr='lerna run --stream --scope $(node -p "require(\"./package.json\").name")'
+
+  # runs "npm run build" (build + test) for the current module
+  alias lb='lr build'
+  alias lt='lr test'
+
+  # runs "npm run watch" for the current module (recommended to run in a separate terminal session)
+  alias lw='lr watch'
 }
