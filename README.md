0f3da4a8ae6eb1b36bf941b40d7ccc90f443f539
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-10-01 22:07:37 -0400

    feat(api): flush out the functionality (#2)

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ### init.zsh:
+- p6df::modules::awscdk::aliases()
 - p6df::modules::awscdk::deps()
+- p6df::modules::awscdk::docker::build()
+- p6df::modules::awscdk::docker::run(...)
 - p6df::modules::awscdk::external::brew()
 - p6df::modules::awscdk::home::symlink()
 - p6df::modules::awscdk::init()
+- p6df::modules::awscdk::langs()
 - p6df::modules::awscdk::version()
 
--- init.zsh
+++ init.zsh
@@ -14,7 +14,7 @@ p6df::modules::awscdk::version() { echo "0.0.1" }
 #
 #>
 ######################################################################
-p6df::modules::awscdk::deps() { ModuleDeps=() }
+p6df::modules::awscdk::deps() { ModuleDeps=(aws/aws-cdk) }
 
 ######################################################################
 #<
@@ -24,6 +24,18 @@ p6df::modules::awscdk::deps() { ModuleDeps=() }
 #>
 ######################################################################
 p6df::modules::awscdk::external::brew() { }
+
+######################################################################
+#<
+#
+# Function: p6df::modules::awscdk::langs()
+#
+#>
+######################################################################
+p6df::modules::awscdk::langs() {
+
+  (cd $P6_DFZ_SRC_DIR/aws/aws-cdk; yarn build)
+}
 
 ######################################################################
 #<
@@ -46,6 +58,13 @@ p6df::modules::awscdk::init() {
   p6df::modules::awscdk::aliases
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::awscdk::aliases()
+#
+#>
+######################################################################
 p6df::modules::awscdk::aliases() {
 
   # runs an npm script via lerna for a the current module
@@ -57,4 +76,32 @@ p6df::modules::awscdk::aliases() {
 
   # runs "npm run watch" for the current module (recommended to run in a separate terminal session)
   alias lw='lr watch'
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::awscdk::docker::build()
+#
+#>
+######################################################################
+p6df::modules::awscdk::docker::build() {
+
+  docker build -t aws-cdk .
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::awscdk::docker::run(...)
+#
+#  Args:
+#	... - 
+#
+#>
+######################################################################
+p6df::modules::awscdk::docker::run() {
+  shift 0
+
+  docker run -v $(pwd):/app -w /app aws-cdk "$@"
 }
