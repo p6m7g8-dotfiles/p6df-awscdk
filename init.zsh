b1a4877ec898e4adc77d5cefe81cc6d284ea260b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-19 12:19:10 -0400

    chore: chases dependencie renames

--- init.zsh
+++ init.zsh
@@ -67,7 +67,7 @@ p6df::modules::awscdk::langs::awscdk() {
 ######################################################################
 p6df::modules::awscdk::clones() {
 
-  p6_run_parallel "0" "4" "$(cat $P6_DFZ_SRC_P6M7G8_DIR/p6df-awscdk/conf/cdks)" "p6_github_util_repo_clone_or_pull_no_ou" "$P6_DFZ_SRC_FOCUSED_DIR"
+  p6_run_parallel "0" "4" "$(cat $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-awscdk/conf/cdks)" "p6_github_util_repo_clone_or_pull_no_ou" "$P6_DFZ_SRC_FOCUSED_DIR"
 }
 
 ######################################################################
