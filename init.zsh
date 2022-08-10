058822e64d1b5f387170a7679e5fcb35e88595ed
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-08-10 15:53:14 -0400

    style: p6ify

--- init.zsh
+++ init.zsh
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 ######################################################################
 #<
 #
@@ -25,6 +26,8 @@ p6df::modules::awscdk::external::brew() {
   brew tap isen-ng/dotnet-sdk-versions
   brew install --cask dotnet-sdk3-1-400
   brew install dotnet
+
+  p6_return_void
 }
 
 ######################################################################
@@ -32,7 +35,6 @@ p6df::modules::awscdk::external::brew() {
 #
 # Function: p6df::modules::awscdk::home::symlink()
 #
-#  Depends:	 p6_file
 #  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
 #>
 ######################################################################
@@ -41,6 +43,8 @@ p6df::modules::awscdk::home::symlink() {
   p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-awscdk/share/.nuget" ".nuget"
   p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-awscdk/share/.dotnet" ".dotnet"
   p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-awscdk/share/.templateengine" ".templateengine"
+
+  p6_return_void
 }
 
 ######################################################################
@@ -54,6 +58,8 @@ p6df::modules::awscdk::langs() {
 
   p6_js_npm_global_install "aws-cdk"
   p6_js_npm_global_install "cdk-dia"
+
+  p6_return_void
 }
 
 ######################################################################
@@ -67,6 +73,8 @@ p6df::modules::awscdk::langs() {
 p6df::modules::awscdk::langs::awscdk() {
 
   p6_run_dir "$P6_DFZ_SRC_DIR/aws/aws-cdk" "yarn build"
+
+  p6_return_void
 }
 
 ######################################################################
@@ -74,25 +82,14 @@ p6df::modules::awscdk::langs::awscdk() {
 #
 # Function: p6df::modules::awscdk::clones()
 #
-#  Depends:	 p6_run
-#  Environment:	 P6_DFZ_SRC_P6M7G8_DIR
+#  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
 #>
 ######################################################################
 p6df::modules::awscdk::clones() {
 
   p6_run_parallel "0" "4" "$(cat $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-awscdk/conf/cdks)" "p6_github_util_repo_clone_or_pull_no_ou" "$P6_DFZ_SRC_FOCUSED_DIR"
-}
 
-######################################################################
-#<
-#
-# Function: p6df::modules::awscdk::prompt::line()
-#
-#>
-######################################################################
-p6df::modules::awscdk::prompt::line() {
-
-  p6_awscdk_prompt_info
+  p6_return_void
 }
 
 ######################################################################
@@ -105,6 +102,8 @@ p6df::modules::awscdk::prompt::line() {
 p6df::modules::awscdk::docker::build() {
 
   docker build -t aws-cdk .
+
+  p6_return_void
 }
 
 ######################################################################
@@ -113,7 +112,7 @@ p6df::modules::awscdk::docker::build() {
 # Function: p6df::modules::awscdk::docker::run(...)
 #
 #  Args:
-#	... -
+#	... - 
 #
 #>
 ######################################################################
@@ -121,4 +120,18 @@ p6df::modules::awscdk::docker::run() {
   shift 0
 
   docker run -v $(pwd):/app -w /app aws-cdk "$@"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::awscdk::prompt::line()
+#
+#>
+######################################################################
+p6df::modules::awscdk::prompt::line() {
+
+  p6_awscdk_prompt_info
 }
